dbb570eb04603d3aec99fa5e3a4df99ea0a04198
edward <imanolopez3103@gmail.com>
2025-04-26 13:26:01 -0500

    Add deps and config to enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: node({
+    mode: 'standalone'
+  }),
+  session: {
+    driver: "fs-lite"
+  },
+  output: "server"
+});
