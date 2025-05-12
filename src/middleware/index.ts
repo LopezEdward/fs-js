b03f329f68969f1b847e8620d696317d908c4ef6
edward <imanolopez3103@gmail.com>
2025-05-11 22:52:51 -0500

    Remove unused middleware

--- src/middleware/index.ts
+++
@@ -1,5 +0,0 @@
-import type { APIContext, MiddlewareNext } from "astro";
-
-function onRequest (context: APIContext, next: MiddlewareNext): Promise<Response> | Response | Promise<void> | void {
-    
-}
